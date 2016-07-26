06d2b757e9155a077dd9b907fce4b50058b3f1d0
sachin lamba <sachin.lamba@moldedbits.com>
2016-07-26 18:16:10 +0530

    passing data successful.

--- UserSignUp/ViewController.swift
+++ UserSignUp/ViewController.swift
@@ -8,18 +8,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var emailID: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var pickedimage: UIImageView!
+    let pickImage = UIImagePickerController()
 
+    @IBAction func ImagePickButton(sender: AnyObject) {
+        presentViewController(pickImage, animated: true, completion: nil)
+    }
+    
+    @IBAction func signUpTapped(sender: AnyObject) {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let newVC = storyBoard.instantiateViewControllerWithIdentifier("signInfo") as! signInfo
+       // newVC.selectedColorFromPreviousScreen = colors[selectedRow]
+        newVC.signUsername = userName.text!
+        newVC.signPhoneNumber = phoneNumber.text
+        newVC.signEmailId = emailID.text
+        newVC.signPassword = password.text
+        newVC.signPic = pickedimage.image
+        presentViewController(newVC, animated: true, completion: nil)
+
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        userName.delegate = self
+        phoneNumber.delegate = self
+        emailID.delegate = self
+        password.delegate = self
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(keyboardReturn)))
+        
+        pickImage.delegate = self
+        
+        
+    }
+    
+    func imagePickerController(imager: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+
+        let newImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        pickedimage.image = newImage
+        dismissViewControllerAnimated(true, completion: nil)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func keyboardReturn() {
+        //textField.resignFirstResponder()
+        
     }
-
 
 }
 
+
+extension ViewController: UITextFieldDelegate {
+//    func textFieldShouldReturn(textField: UITextField) -> Bool {
+//        if textField.text?.characters.count == 10 {
+//            textField.resignFirstResponder()
+//            
+//            return true
+//        }
+//        return false
+//    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        
+    }
+    
+//    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+//        return textField.text?.characters.count == 10
+//    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        
+    }
+
+}
+
+//extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+//    //let pickImage: UIImagePickerController()
+//    
+//    
+//    
+//}
---
+++ UserSignUp/signInfo.swift
@@ -0,0 +1,63 @@
+//
+//  signInfo.swift
+//  UserSignUp
+//
+//  Created by Sachin Lamba on 26/07/16.
+//  Copyright © 2016 LambaG. All rights reserved.
+//
+
+import UIKit
+
+class signInfo: UIViewController {
+
+    var signUsername: String!
+    var signPhoneNumber: String!
+    var signEmailId: String!
+    var signPassword: String!
+    var signPic: UIImage!
+    
+    @IBOutlet weak var userSignUp: UITextField!
+    @IBOutlet weak var phoneSignUp: UITextField!
+    @IBOutlet weak var emailIdSignUp: UITextField!
+    @IBOutlet weak var passwordSignUp: UITextField!
+    @IBOutlet weak var picSignUp: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor.yellowColor()
+        
+        //let sign = signUsername
+        userSignUp.text = signUsername
+        phoneSignUp.text = signPhoneNumber
+        emailIdSignUp.text = signEmailId
+        passwordSignUp.text = signPassword
+        //picSignUp.frame = CGRect(x: 50, y: 300, width: 80, height: 80)
+        picSignUp.image = signPic
+        
+        
+
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
